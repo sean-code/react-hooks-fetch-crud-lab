47846da73fea5aac23a65e70bf6d6d3baa77551e
sean-code <john.nganga@student.moringaschool.com>
2022-09-27 12:17:51 +0300

    Finished Lab

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -12,10 +12,41 @@ function QuestionList() {
      setQuestions(questions));
   }, []);
 
+  function handleDeleteClick(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => {
+        const updatedQuestions = questions.filter((q) => q.id !== id);
+        setQuestions(updatedQuestions);
+      });
+  }
+
+  function handleAnswerChange(id, correctIndex) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex }),
+    })
+      .then((r) => r.json())
+      .then((updatedQuestion) => {
+        const updatedQuestions = questions.map((q) => {
+          if (quest.id === updatedQuestion.id) return updatedQuestion;
+          return quest;
+        });
+        setQuestions(updatedQuestions);
+      });
+  }
+
   const questionItems = questions.map((quest) => (
     <QuestionItem
       key={quest.id}
       question={quest}
+      onDeleteClick={handleDeleteClick}
+      onAnswerChange={handleAnswerChange}
     />
   ));
 
