069706503e0adc9005b75177c876a810a0bdb651
sean-code <john.nganga@student.moringaschool.com>
2022-09-27 11:39:41 +0300

    Add GET Request

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+  // const [prompt, setPrompt] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+    .then((response) => response.json())
+    .then((questions) =>
+     setQuestions(questions));
+  }, []);
+
+  const questionItems = questions.map((quest) => (
+    <QuestionItem
+      key={quest.id}
+      question={quest}
+    />
+  ));
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul> {questionItems}  </ul>
     </section>
   );
 }
